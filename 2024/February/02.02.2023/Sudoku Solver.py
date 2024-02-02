366d92b847377edf5c585f4e09ad1262678eea6d
Arsen Butko <arseny.butko771@gmail.com>
2024-02-02 19:42:55 +0300

    02.02.2024

---
+++ 2024/February/02.02.2023/Sudoku Solver.py
@@ -0,0 +1,79 @@
+"""
+Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+
+The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+
+For Sudoku rules, see the Wikipedia article.
+
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+sudoku(puzzle)
+# Should return
+ [[5,3,4,6,7,8,9,1,2],
+  [6,7,2,1,9,5,3,4,8],
+  [1,9,8,3,4,2,5,6,7],
+  [8,5,9,7,6,1,4,2,3],
+  [4,2,6,8,5,3,7,9,1],
+  [7,1,3,9,2,4,8,5,6],
+  [9,6,1,5,3,7,2,8,4],
+  [2,8,7,4,1,9,6,3,5],
+  [3,4,5,2,8,6,1,7,9]]
+"""
+
+
+def sudoku(puzzle: list) -> list:
+    """Solve the 9x9 Sudoku puzzle using a backtracking algorithm"""
+
+    def is_valid(puzzle, row, col, num):
+        # Check if the number is already used in the row
+        if num in puzzle[row]:
+            return False
+
+        # Check if the number is already used in the column
+        if num in [puzzle[i][col] for i in range(9)]:
+            return False
+
+        # Check if the number is already used in the 3x3 subgrid
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if puzzle[start_row + i][start_col + j] == num:
+                    return False
+
+        return True
+
+    def solve():
+        for row in range(9):
+            for col in range(9):
+                if puzzle[row][col] == 0:
+                    for num in range(1, 10):
+                        if is_valid(puzzle, row, col, num):
+                            puzzle[row][col] = num
+                            if solve():
+                                return True
+                            puzzle[row][col] = 0  # Backtrack
+                    return False
+        return True
+
+    solve()
+    return puzzle
+
+
+if __name__ == "__main__":
+    print(sudoku([[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                  [0, 0, 0, 0, 8, 0, 0, 7, 9]]))
--- test.py
+++ test.py
@@ -1,16 +1,2 @@
 
 
-def grep(pattern):
-    print("Searching for", pattern)
-    while True:
-        line = (yield)
-        if pattern in line:
-            print(line)
-
-
-search = grep('coroutine')
-next(search)
-
-search.send("I love you")
-search.send("Don't you love me?")
-search.send("I love coroutines instead!")
