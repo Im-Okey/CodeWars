c520732e853e6915d379dabf24e1413fd8d0832e
Im Okey <arseny.butko771@gmail.com>
2024-10-25 22:37:57 +0300

    leetcode easy - #20

---
+++ leet_code/easy/#20 Valid Parentheses.py
@@ -0,0 +1,22 @@
+"""
+Given a string s containing just the characters
+'(', ')', '{', '}', '[' and ']', determine if
+the input string is valid.
+"""
+
+
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
+
+
+if __name__ == '__main__':
+    print(isValid("()[]{}"))
